dec2962d328d9dd341fd26b6fd12d2c87f641d5a
seungmin shin <greedy0110@texclub.com>
2019-01-24 13:32:41 +0900

    first commit

--- BackgroundSettingsEx/Form1.Designer.cs
+++ BackgroundSettingsEx/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(197, 227);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(75, 23);
+            this.changeButton.TabIndex = 0;
+            this.changeButton.Text = "change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.changeButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button changeButton;
     }
 }
 
--- BackgroundSettingsEx/Form1.cs
+++ BackgroundSettingsEx/Form1.cs
@@ -12,9 +12,40 @@ namespace BackgroundSettingsEx
 {
     public partial class Form1 : Form
     {
+        Color[] colorList = new Color[] { Color.White, Color.Yellow, Color.Green, Color.Black, Color.Blue };
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+
+            // Settings 읽기
+            BackColor = Properties.Settings.Default.Background;
+
+            // Settings 변경시 이벤트 등록해서 gui 실시간 반영
+            Properties.Settings.Default.SettingsSaving += Default_SettingsSaving;
+
+            // SettingsSaving => 저장 된 이후
+            // SettingChanging => 저장 할 건데, 저장하기 전 값으로 한다.
+        }
+
+        private void Default_SettingsSaving(object sender, CancelEventArgs e)
+        {
+            // Settings 읽기
+            BackColor = Properties.Settings.Default.Background;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Settings 변경시 이벤트 등록해제 해야 한다.
+            Properties.Settings.Default.SettingsSaving -= Default_SettingsSaving;
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            // Settings 쓰기
+            Properties.Settings.Default.Background = colorList[random.Next() % colorList.Length];
+            Properties.Settings.Default.Save();
         }
     }
 }
